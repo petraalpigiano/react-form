8227deb2341b2fff0e2a8d3bf841192816a2806a
petraalpigiano <petraalpigiano@gmail.com>
2025-05-26 16:03:06 +0200

    add list

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import blogPosts from "./data/blogPosts.js";
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container text-center my-3">
+        {/* LISTA DI TITOLI */}
+        {blogPosts.map(function (currentTitle, index) {
+          return <h3 key={index}>{currentTitle}</h3>;
+        })}
+      </div>
+    </>
+  );
 }
---
+++ src/data/blogPosts.js
@@ -0,0 +1,3 @@
+const blogPosts = ["Titolo 1", "Titolo 2", "Titolo 3", "Titolo 4", "Titolo 5"];
+
+export default blogPosts;
